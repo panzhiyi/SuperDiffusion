3d2992573db854ba5a38cd0670968d3099aa9c47
Zhiyi Pan <panzhiyi1996@gmail.com>
2020-04-30 23:40:07 +0800

    Delete normU1.m

--- others/normU1.m
+++
@@ -1,7 +0,0 @@
-function [U,sv]=normU1(U0,sv)
-[sp_num,~]=size(U0);
-fsal = U0*U0'*sv;
-fsal_min=min(fsal);
-fsal_max=max(fsal);
-U1=sqrt(-fsal_min/((fsal_max-fsal_min)));
-U=[U1*ones(sp_num,1) U0/sqrt(fsal_max-fsal_min)];
